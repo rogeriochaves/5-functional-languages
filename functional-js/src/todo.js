2dc5aa8f9550c083e28ce7bfb0d453efeb89e6c6
Rogerio Chaves <rogerio.chaves@booking.com>
2022-05-08 21:08:15 +0200

    Revert "Remove all this"
    This reverts commit 4419ba9575807732caeea4e5615bbd357dd6f792.

--- functional-js/src/todo.js
+++ functional-js/src/todo.js
@@ -1,85 +1,73 @@
-const main = () => {
-  let todoList = document.querySelector("#app");
-  let todoInput = document.querySelector("#todo-input");
-
-  let state = {
-    todos: [],
-  };
-
-  class TodoList {
-    constructor() {
-      new TodoInput(todoList);
-    }
-
-    static addTodo(text) {
-      new Todo(text);
-      TodoList.reorderTodos();
-    }
-
-    static reorderTodos() {
-      state.todos = state.todos.sort((a, b) => a.done - b.done);
-      for (const todo of state.todos) {
-        todoList.removeChild(todo.el);
-        todoList.appendChild(todo.el);
-      }
-    }
+class TodoList {
+  constructor() {
+    this.el = document.querySelector("#app");
+    this.todoInput = new TodoInput(this);
+    this.todos = [];
   }
 
-  class TodoInput {
-    constructor() {
-      todoInput.addEventListener("keyup", TodoInput.onKeyUp);
-    }
-
-    static onKeyUp(e) {
-      const ENTER_KEY = 13;
-      if (e.keyCode == ENTER_KEY) {
-        TodoList.addTodo(todoInput.value);
-        todoInput.value = "";
-      }
-    }
+  addTodo(text) {
+    const todo = new Todo(this);
+    todo.setText(text);
+    this.todos.unshift(todo);
+    this.el.appendChild(todo.el);
+    this.reorderTodos();
   }
 
-  class Todo {
-    constructor(text) {
-      let todo = {};
-      todo.done = false;
-
-      todo.todoText = document.createElement("span");
-
-      todo.doneButton = document.createElement("a");
-      todo.doneButton.href = "#";
-      todo.doneButton.innerText = "Done";
-      todo.doneButton.style.paddingLeft = "5px";
-
-      todo.el = document.createElement("div");
-      todo.el.appendChild(todo.todoText);
-      todo.el.appendChild(todo.doneButton);
-      todoList.appendChild(todo.el);
-
-      todo.doneButton.addEventListener("click", Todo.setDone(todo));
-
-      Todo.setText(todo, text);
-
-      state.todos.unshift(todo);
-    }
-
-    static setText(todo, text) {
-      todo.todoText.innerText = text;
-    }
-
-    static setDone(todo) {
-      return () => {
-        todo.el.removeChild(todo.doneButton);
-        todo.todoText.style.textDecoration = "line-through";
-        todo.done = true;
-        TodoList.reorderTodos();
-      };
+  reorderTodos() {
+    this.todos = this.todos.sort((a, b) => a.done - b.done);
+    for (const todo of this.todos) {
+      this.el.removeChild(todo.el);
+      this.el.appendChild(todo.el);
     }
   }
+}
 
-  new TodoList();
-};
+class TodoInput {
+  constructor(todoList) {
+    this.el = document.querySelector("#todo-input");
+    this.el.addEventListener("keyup", this.onKeyUp.bind(this));
+    this.todoList = todoList;
+  }
+
+  onKeyUp(e) {
+    const ENTER_KEY = 13;
+    if (e.keyCode == ENTER_KEY) {
+      this.todoList.addTodo(this.el.value);
+      this.el.value = "";
+    }
+  }
+}
+
+class Todo {
+  constructor(todoList) {
+    this.todoList = todoList;
+    this.done = false;
+
+    this.todoText = document.createElement("span");
+
+    this.doneButton = document.createElement("a");
+    this.doneButton.href = "#";
+    this.doneButton.innerText = "Done";
+    this.doneButton.style.paddingLeft = "5px";
+    this.doneButton.addEventListener("click", this.setDone.bind(this));
+
+    this.el = document.createElement("div");
+    this.el.appendChild(this.todoText);
+    this.el.appendChild(this.doneButton);
+  }
+
+  setText(text) {
+    this.todoText.innerText = text;
+  }
+
+  setDone() {
+    this.el.removeChild(this.doneButton);
+    this.todoText.style.textDecoration = "line-through";
+    this.done = true;
+    this.todoList.reorderTodos();
+  }
+}
 
 export default () => {
-  main();
+  new TodoList();
 };
